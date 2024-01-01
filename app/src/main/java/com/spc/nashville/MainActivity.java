5ffdaae77ddb5590f755d73a70d5372bc46b54d6
shaun <shauncotter00@gmail.com>
2024-01-01 21:57:39 +0000

    upgraded to sdk 34

--- app/src/main/java/com/spc/nashville/MainActivity.java
+++ app/src/main/java/com/spc/nashville/MainActivity.java
@@ -4,7 +4,7 @@ import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
